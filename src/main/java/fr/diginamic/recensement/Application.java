0f9d1d93e17b10ef63807c8018f8645a2ee2af34
ricbon35 <ricbon35@gmail.com>
2019-10-02 16:10:14 +0200

    UTF-8

--- src/main/java/fr/diginamic/recensement/Application.java
+++ src/main/java/fr/diginamic/recensement/Application.java
@@ -9,13 +9,13 @@ import org.apache.commons.io.FileUtils;
 import fr.diginamic.recensement.entites.Recensement;
 import fr.diginamic.recensement.entites.Ville;
 
-/** Application de traitement des données de recensement de population
+/** Application de traitement des donnÃ©es de recensement de population
  * @param args
  */
 public class Application {
 	
-	/** Point d'entrée
-	 * @param args arguments (non utilisés ici)
+	/** Point d'entrÃ©e
+	 * @param args arguments (non utilisÃ©s ici)
 	 */
 	public static void main(String[] args) {
 		Recensement recensement = new Recensement();
@@ -25,7 +25,7 @@ public class Application {
 			File file = new File("C:/temp/recensement population 2016.csv");
 			lignes = FileUtils.readLines(file, "UTF-8");
 			
-			// On supprime la ligne d'entête avec les noms des colonnes
+			// On supprime la ligne d'entÃ©te avec les noms des colonnes
 			lignes.remove(0);
 			
 			for (String ligne: lignes){
@@ -37,10 +37,10 @@ public class Application {
 				String nomCommune = morceaux[6];
 				int populationTotale = Integer.parseInt(morceaux[9].replace(" ", "").trim());
 				
-				// On cree maintenant la ville avec toutes ses données
+				// On cree maintenant la ville avec toutes ses donnÃ©es
 				Ville ville = new Ville(codeRegion, nomRegion, codeDepartement, codeCommune, nomCommune, populationTotale);
 				
-				// On ajoute la ville à la liste des villes du recensement
+				// On ajoute la ville Ã  la liste des villes du recensement
 				recensement.getVilles().add(ville);
 			}
 			
