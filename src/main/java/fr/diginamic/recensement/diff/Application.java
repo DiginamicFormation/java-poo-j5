4a8f7d19ac5bc0ab9232b4751d3c84fef5f18cdf
ricbon35 <ricbon35@gmail.com>
2019-11-18 15:48:12 +0100

    Refactoring + nv tp normal

--- src/main/java/fr/diginamic/recensement/diff/Application.java
+++ src/main/java/fr/diginamic/recensement/diff/Application.java
@@ -1,4 +1,4 @@
-package fr.diginamic.recensement;
+package fr.diginamic.recensement.diff;
 
 import java.io.File;
 import java.io.IOException;
---
+++ src/main/java/fr/diginamic/recensement/normal/Application.java
@@ -0,0 +1,41 @@
+package fr.diginamic.recensement.normal;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+/**
+ * Application de traitement des données de recensement de population
+ * 
+ * @param args
+ */
+public class Application {
+
+	/**
+	 * Point d'entrée
+	 * 
+	 * @param args
+	 *            arguments (non utilisés ici)
+	 */
+	public static void main(String[] args) {
+
+		List<String> lignes = null;
+		try {
+			File file = new File("C:/temp/recensement population 2016.csv");
+			lignes = FileUtils.readLines(file, "UTF-8");
+
+			// On supprime la ligne d'entéte avec les noms des colonnes
+			lignes.remove(0);
+
+			for (String ligne : lignes) {
+				System.out.println(ligne);
+			}
+
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+}
