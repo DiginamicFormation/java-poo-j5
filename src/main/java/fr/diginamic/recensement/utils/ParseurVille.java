cd0e68d9cc69a3b6c125d910314cd3cca1746871
ricbon35 <ricbon35@gmail.com>
2019-11-18 14:03:07 +0100

    Correctif Parseur

--- src/main/java/fr/diginamic/recensement/utils/ParseurVille.java
+++ src/main/java/fr/diginamic/recensement/utils/ParseurVille.java
@@ -21,7 +21,7 @@ public class ParseurVille {
 		String codeDepartement = morceaux[2];
 		String codeCommune = morceaux[5];
 		String nomCommune = morceaux[6];
-		int populationTotale = Integer.parseInt(morceaux[9].replace(" ", "").trim());
+		int populationTotale = Integer.parseInt(morceaux[7].replace(" ", "").trim());
 		
 		// On cree maintenant la ville avec toutes ses données
 		Ville ville = new Ville(codeRegion, nomRegion, codeDepartement, codeCommune, nomCommune, populationTotale);
